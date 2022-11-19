a38c0262a4b792f74b54022b8e6e73828b264277
mustafaarii <mustafaarii161@gmail.com>
2022-11-19 17:27:53 +0300

    responsive  fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,65 @@
-function toggleNavbarButton(){
-    const nav = document.getElementById("nav_menu");
-    let display = nav.style.display;
-    
-    if(display === 'none')
-        nav.style.display = 'block';
+//selectors
+const navbar = document.getElementsByClassName("navbar")[0];
+const navmenu = document.getElementById("nav_menu");
+const sidebar = document.getElementsByClassName("sidebar")[0];
+const bodyContainer = document.getElementsByClassName("body-container")[0];
+const categoriesButtons = document.querySelector(".body-container .categories-buttons");
+const loginButton = document.querySelector(".login_button");
+
+//variables
+let oldScrollY = window.scrollY;
+
+//events
+function toggleNavbarButton() {
+    let display = navmenu.style.display;
+
+    if (display === 'none')
+        navmenu.style.display = 'block';
     else
-    nav.style.display = 'none';    
+        navmenu.style.display = 'none';
 
 }
 
 function toggleHamburgerButton() {
-    const sidebar = document.getElementsByClassName("sidebar")[0];
-    
-    if(sidebar.classList.contains("closed"))
+    if (sidebar.classList.contains("closed"))
         sidebar.classList.remove("closed")
-    else    
+    else
         sidebar.classList.add("closed")
 
 }
 
 function closeNavbarButton() {
-    const nav = document.getElementById("nav_menu");
-    nav.style.display = 'none';
+    navmenu.style.display = 'none';
 }
 
-window.onload = function onloadFunc(e){
-    const container = document.getElementsByClassName("body-container")[0];
-    container.addEventListener("click", closeNavbarButton)    
+window.onload = function onloadFunc(e) {
+    bodyContainer.addEventListener("click", closeNavbarButton)
 };
+
+// mobile actions
+
+if (window.screen.width < 600) {
+    window.onscroll = function onScroll(e) {
+
+        if (oldScrollY < window.scrollY)
+            translateVal = -100;
+        else
+            translateVal = 0;
+
+        navbar.style.transform = "translateY(" + translateVal + "%)";
+        categoriesButtons.style.transform = "translateY(" + translateVal * 2 + "%)";
+
+        oldScrollY = window.scrollY;
+    }
+
+
+    loginButton.onclick = function onClickLoginButton(e) {
+        let display = sidebar.style.display;
+
+        if (display === 'none')
+            sidebar.style.display = 'block';
+        else
+            sidebar.style.display = 'none';
+
+    }
+}
